90ff1e89cbf3964ffa76a95c077fa20f7ed0109a
301 <301@223.194.169.97>
2015-03-20 12:54:31 +0900

    d

--- JSPProject/src/exam/first/HelloServlet.java
+++ JSPProject/src/exam/first/HelloServlet.java
@@ -29,10 +29,17 @@ public class HelloServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		
+		//get방식은 주소창을 타고 넘어가기 때문에 요청하는 데이터를 사용자가 그대로 볼 수 있어 보안에 취약
+		response.setContentType("text/html; charset=UTF-8");
 		PrintWriter out = response.getWriter();
 		
-		out.print("Hello!");
+		String n = request.getParameter("name");
+		String i = request.getParameter("id");
+		String p = request.getParameter("pwd");
+		out.print("hello - GET<br>");
+		out.print("이름 : " + n + "<br>");
+		out.print("아이디 : " + i + "<br>");
+		out.print("비밀번호 : " + p + "<br>");
 	}
 
 	/**
@@ -40,6 +47,18 @@ public class HelloServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		//post방식은 html 헤더를 타고 넘어가기 때문에 보안에 강하다
+		response.setContentType("text/html charset=UTF-8");
+		request.setCharacterEncoding("UTF-8");//set은 설정해준다.
+		PrintWriter out = response.getWriter();
+		
+		String n = request.getParameter("name");
+		String i = request.getParameter("id");
+		String p = request.getParameter("pwd");
+		out.print("hello - POST<br>");
+		out.print("이름 : " + n + "<br>");
+		out.print("아이디 : " + i + "<br>");
+		out.print("비밀번호 : " + p + "<br>");
 	}
 
 }
